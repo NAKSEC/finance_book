bac312e6cbf51c32574dee95c3f716a07e2e69e6
Gal Nakash <nakashgal@gmail.com>
2019-05-26 09:46:18 -0400

    add files

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+docker run -d -p 27017-27019:27017-27019 --name mongodb mongo:4.0.4
+virtualenv --python=python2.7 .env && source .env/bin/activate && pip install -r requirements.txt
 cd fin_scrapy
-scrapy crawl -a ticker=AAPL --nolog -o - -t json finance
+scrapy crawl -a tickers=AAPL --nolog -o - -t json finance
 
