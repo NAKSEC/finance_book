4a0cf51e6ff465e20efe4f417c019bd6d6e37d7a
Gal Nakash <nakashgal@gmail.com>
2019-05-28 11:37:38 -0400

    fix balance sheet bug

--- fin_scrapy/financestats/spiders/parser.py
+++ fin_scrapy/financestats/spiders/parser.py
@@ -11,10 +11,10 @@ def format_earnings_list(earnings_raw):
 
 def parse_balance_sheet(json_raw_result, ticker):
     balance_sheets = []
-    balance_sheet = json_raw_result["balanceSheetHistory"]["balanceSheetStatements"][0]
+    balance_sheet = json_raw_result["balanceSheetHistory"]["balanceSheetStatements"]
     print "len %s" % len(balance_sheet)
     for i in balance_sheet:
-        balance_sheet_data = utils.raw_json_answer_parser(balance_sheet)
+        balance_sheet_data = utils.raw_json_answer_parser(i)
         balance_sheet_data['ticker'] = ticker
         balance_sheets.append(balance_sheet_data)
     return balance_sheets
