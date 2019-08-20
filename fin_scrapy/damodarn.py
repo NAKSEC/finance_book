c0134cdacdd694b5ad3708aa2e169e5695e1a093
galnakash <nakashgal@gmail.com>
2019-08-20 16:49:35 +0300

    Added damodarn ERP by country

--- fin_scrapy/damodarn.py
+++ fin_scrapy/damodarn.py
@@ -6,18 +6,19 @@ import xlrd
 
 from fin_scrapy.utils.mongohandler import *
 
-DAMODARN_URL = "http://www.stern.nyu.edu/~adamodar/pc/datasets/betas.xls"
+DAMODARN_URL = "http://www.stern.nyu.edu/~adamodar/pc/datasets/"
+BETAS = "betas.xls"
 MONGO_ADDRESS = 'mongodb://localhost:27017/'
-
 MONGO_CONNECTION = Mongo_Client_Wrapper(MONGO_ADDRESS)
+COUNTRYֹֹּ_PREMIUM = "ctryprem.xls"
 
 
-def get_titles_row(row_values):
+def get_titles_row(title, row_values):
     is_row = False
     titles = []
     for val in row_values:
         titles.append(val)
-        if "Industry Name" == val:
+        if title == val:
             is_row = True
 
     if is_row == False:
@@ -25,10 +26,10 @@ def get_titles_row(row_values):
     return titles
 
 
-def parse_damodarn_data_set(file_path):
+def parse_damodarn_data_set(file_path, title_to_parse, sheet):
     # Open the workbook and select the first worksheet
     wb = xlrd.open_workbook(file_path)
-    sh = wb.sheet_by_index(0)
+    sh = wb.sheet_by_name(sheet)
     # List to hold dictionaries
     industry_data = []
     keys = []
@@ -37,7 +38,7 @@ def parse_damodarn_data_set(file_path):
     for rownum in range(1, sh.nrows):
 
         row_values = sh.row_values(rownum)
-        titles = get_titles_row(row_values)
+        titles = get_titles_row(title_to_parse, row_values)
         if titles is not None:
             keys = titles
             started_row = rownum + 1
@@ -55,13 +56,30 @@ def parse_damodarn_data_set(file_path):
     return industry_data
 
 
-def download_damodarn_dataset():
-    r = requests.get(DAMODARN_URL)
-    temp_file_name = '.{}'.format(hash(os.times()))
-    if r.status_code == 200:
-        with open(temp_file_name, "wb") as f:
-            f.write(r.content)
-        data = parse_damodarn_data_set(temp_file_name)
+def write_data_to_file(temp_file_name, buffer):
+    with open(temp_file_name, "wb") as f:
+        f.write(buffer)
+
+
+def insert_dict_to_database(data):
+    for industry in data:
+        MONGO_CONNECTION.mongo_insert("damodarn", industry)
+
+
+def download_damodarn_dataset(file_name_from_server, first_row_title, sheet_name):
+    request = requests.get(DAMODARN_URL + file_name_from_server)
+    temp_file_name = '{}'.format(hash(os.times()))
+    if request.status_code == 200:
+        write_data_to_file(temp_file_name, request.content)
+        data = parse_damodarn_data_set(temp_file_name, first_row_title, sheet_name)
         os.remove(temp_file_name)
-        for i in data:
-            MONGO_CONNECTION.mongo_insert("beta", i)
+        insert_dict_to_database(data)
+
+
+def download_all_data_from_damodarn():
+    download_damodarn_dataset(BETAS, "Industry Name", "Sheet1")
+    download_damodarn_dataset(COUNTRYֹֹּ_PREMIUM, "Country", "ERPs by country")
+
+
+if __name__ == '__main__':
+    download_all_data_from_damodarn()
